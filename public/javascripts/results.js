aa9417d594c90fa33d1e6a3317cc2e4f994ea24d
EdGuz <edguzman2408@gmail.com>
2016-02-10 14:36:52 -0400

    Update results.js

--- public/javascripts/results.js
+++ public/javascripts/results.js
@@ -77,6 +77,7 @@ function showWinners(){
     var currentCategory;
     var currentUser;
     var total;
+    var summary = {};
     
     console.log(result);	
     
@@ -103,6 +104,20 @@ function showWinners(){
                   tableContent += '<td>$' + parseInt(value.amount / value.voters) + '</td>';
                   tableContent += '</tr>';    
                   
+                  total = parseInt(value.amount / value.voters);
+                  Summary[item.user] = total;
+                  
+                  /*SummaryTable += '<div style="float:left; margin-left:15px;">';
+                  SummaryTable += '<h4>Summary</h4>';
+                  SummaryTable += '<table class="table table-hover table-bordered">';
+                  SummaryTable += '<tr>';
+                  SummaryTable += '<th>User</th>';
+                  SummaryTable += '<th>Total Won</th>';
+                  SummaryTable += '</tr>';
+                  SummaryTable += '<tr>';
+                  SummaryTable += '<td>' + item.user + '</td>';
+                  SummaryTable += '<td>' + total + '</td>';*/
+                  
                   currentCategory = value.category;
                   currentUser = item.user;
                   
@@ -112,41 +127,23 @@ function showWinners(){
                   tableContent += '<td>' + item.user + '</td>';
                   tableContent += '<td>$' + parseInt(value.amount / value.voters) + '</td>';
                   tableContent += '</tr>';                       
+                  total += parseInt(value.amount / value.voters);
+                  Summary[item.user] = total;
                   
-                  //tableContent += SummaryTable;
+                  /*tableContent += SummaryTable;
+                  total += parseInt(value.amount / value.voters);
+                  tableContent += '<tr>';
+                  tableContent += '<td>' + item.user + '</td>';
+                  tableContent += '<td>' + total + '</td>';
+                  tableContent += '</tr>';
                 
-                  //tableContent += '</table></div>';
+                  tableContent += '</table></div>';*/
                   
                 }
                  
   
             }  
-              
-            if(value.winner ==  val.selection){
-              if(value.category != currentCategory && item.user != currentUser){
-                total = parseInt(value.amount / value.voters);
-                SummaryTable += '<div style="float:left; margin-left:15px;">';
-                SummaryTable += '<h4>Summary</h4>';
-                SummaryTable += '<table class="table table-hover table-bordered">';
-                SummaryTable += '<tr>';
-                SummaryTable += '<th>User</th>';
-                SummaryTable += '<th>Total Won</th>';
-                SummaryTable += '</tr>';
-                SummaryTable += '<tr>';
-                SummaryTable += '<td>' + item.user + '</td>';
-                SummaryTable += '<td>' + total + '</td>';
-                
-                currentCategory = value.category;
-                currentUser = item.user;
-                
-              } else {
-                total += parseInt(value.amount / value.voters);
-                SummaryTable += '<tr>';
-                SummaryTable += '<td>' + item.user + '</td>';
-                SummaryTable += '<td>' + total + '</td>';
-                SummaryTable += '</tr>';
-              }
-            }
+            
             
           });
             
@@ -154,7 +151,7 @@ function showWinners(){
           });
           
           
-        SummaryTable += '</table></div>';
+        //SummaryTable += '</table></div>';
         tableContent += '</table></div></div>';
         console.log(SummaryTable);
         console.log(tableContent);
@@ -163,9 +160,29 @@ function showWinners(){
       
       
       $('#container').append(tableContent);
-      $('#summary').append(SummaryTable);
+      //$('#summary').append(SummaryTable);
+      
+      
      
    });
+   console.log(summary);
+   
+   $.map(summary, function(n, i) {
+     SummaryTable += '<div>';
+     SummaryTable += '<table class="table">';
+     SummaryTable += '<tr>';
+     SummaryTable += '<th> User </th>';
+     SummaryTable += '<th> Total Won </th>';
+     SummaryTable += '</tr>';
+     SummaryTable += '<tr>';
+     SummaryTable += '<td>' + i + '</td>';
+     SummaryTable += '<td>' + n + '</td>';
+     SummaryTable += '</tr>';
+     SummaryTable += '</table>';
+     SummaryTable += '<div>';
+   });
+    
+   $('#summary').append(SummaryTable);
     
  });
 }
