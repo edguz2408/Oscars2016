25fccd2b4db99b48281f9ce68d86793098a35e8a
Edwin Guzman Marte <EdGuzmanMarte@Edwins-MacBook-Pro.local>
2016-02-21 17:19:57 -0400

    added some other stuff

--- routes/index.js
+++ routes/index.js
@@ -52,7 +52,7 @@ module.exports = function(passport) {
   router.get('/getData', function(req, res) {
     Nominations.find({}, function(err, nominations) {
       if (!err) {
-        var d = new Date("February 19, 2016 17:00:00");
+        var d = new Date("February 28, 2016 17:00:00");
         var n = Date.now();
 
         console.log('Limit Date:: '+ d);
